fd22d5fbedaddf8fa7429538eb0f2812eaabb5a4
KingJaro27 <kingjaro27@gmail.com>
2025-02-25 18:42:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,5 +11,28 @@ def home():
 def index():
     return "И на Марсе будут яблони цвести!"
 
+@app.route("/promotion")
+def promotion():
+    return """<!doctype html>
+                <html lang="en">
+                <head>
+                    <meta charset="utf-8">
+                    <title>Реклама</title>
+                  </head>
+                  <body>
+                    <h1>Человечество вырастает из детства.</h1>
+                    </br>
+                    <h1>Человечеству мала одна планета.</h1>
+                    </br>
+                    <h1>Мы сделаем обитаемыми безжизненные пока планеты.</h1>
+                    </br>
+                    <h1>И начнем с Марса!</h1>
+                    </br>
+                    <h1>Присоединяйся!</h1>
+                </body>
+                </html>
+"""
+
+
 if __name__ == "__main__":
     app.run(port=7016, host="127.0.0.1")
