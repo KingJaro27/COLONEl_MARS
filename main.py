4c69ae08cd4a4eaae9014393ecf9238b5325885b
KingJaro27 <kingjaro27@gmail.com>
2025-02-25 16:33:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/index")
-
-
-def index():
-    return "И на Марсе будут яблони цвести!"
-
 def home():
     return "Миссия Колонизация Марса"
+
+@app.route("/index")
+def index():
+    return "И на Марсе будут яблони цвести!"
 
 if __name__ == "__main__":
     app.run(port=7016, host="127.0.0.1")
